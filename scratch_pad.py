be605c7ab200628fb403ed0256f99aae5d7c1ae0
Sabrina Golonka <sabrina.golonka@ons.gov.uk>
2025-10-02 13:00:47 +0100

    lvu_comp has basic functionality

--- scratch_pad.py
+++ scratch_pad.py
@@ -11,39 +11,30 @@ def create_contingency_table(input_file_name, category_1, category_2):
   contingency_table = pd.crosstab(data[category_1], data[category_2])
 
   # Drops unecessary columns if any (e.g., category labels)
-  contingency_table = data.drop(columns=['Category']).values
-  
+  #contingency_table = data.drop(columns=['Category']).values
+   
   return contingency_table
 
-# Performs a chi square test on specified input file and categorical variables
+#example_contingency = create_contingency_table('data', 'Category', 'LinkedStatus')
+
+#print(example_contingency)
 
 def lvu_chi2(input_file_name, category_1, category_2):
   # Creates contingency table
   contingency_table = create_contingency_table(input_file_name, category_1, category_2)
   
+  print(contingency_table)
+  # this contigency table part is working fine as can print it out
+
   # Performs Chi2 test
+  # This bit is causing a type error when I try to call the lvu_chi2 function
+  # problem was dropping columns in contingency table creation
+
   chi2, p, dof, expected = chi2_contingency(contingency_table)
   
+  print(f"Chi2: {chi2}")
+
   return chi2, p, dof, expected
 
-# Example usage:
-lvu_comparison = lvu_chi2('data', 'Category', 'LinkedStatus')
-print(lvu_comparison)
-
-
-#def lvu_chi2(contingency_table):
-  # Performs Chi2 test
- # chi2, p, dof, expected = chi2_contingency(contingency_table)
-  
-  #return chi2, p, dof, expected
-
-# Performs a chi square test on specified contingency table
-
-#def perform_chi2_test(contingency_table):
-  # Performs Chi2 test
- # chi2, p, dof, expected = chi2_contingency(contingency_table)
-  
- # return chi2, p, dof, expected
-
-
-
+test_chi2 = lvu_chi2('data', 'Category', 'LinkedStatus')
+#print(test_chi2)

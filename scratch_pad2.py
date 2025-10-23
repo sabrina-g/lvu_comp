c3cb3fbf2b4626098a02ae05d43b4b3792f1d644
Sabrina Golonka <sabrina.golonka@ons.gov.uk>
2025-10-23 08:16:12 +0100

    Made category a variable and added to print statements

--- scratch_pad2.py
+++ scratch_pad2.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 input_file_name = 'effect_data1'
 data = pd.read_csv('data/'+input_file_name+'.csv')
+category = 3
 
 # Create separate df for linked and unlinked records
 df_linked = data[data['LinkedStatus'] == 1]
@@ -14,23 +15,23 @@ df_linked_true = df_linked[df_linked['LinkTruth'] == 1]
 # Create separate df of missed matches assuming 0 = false 
 df_unlinked_false = df_unlinked[df_unlinked['LinkTruth'] == 0]
 
-# Create separate df of True matches within specified category, e.g., Category = 1
-df_linked_true_cat_1 = df_linked_true[df_linked_true['Category'] == 1]
+# Create separate df of True matches within specified category
+df_linked_true_cat = df_linked_true[df_linked_true['Category'] == category]
 
-# Create separate df of Missed matches within specified category, e.g., Category = 1
-df_unlinked_false_cat_1 = df_unlinked_false[df_unlinked_false['Category'] == 1]
+# Create separate df of Missed matches within specified category
+df_unlinked_false_cat = df_unlinked_false[df_unlinked_false['Category'] == category]
 
 # Numbers of linked and unlinked records
 linked_n = len(df_linked)
 unlinked_n = len(df_unlinked)
 
 # Number of true matches within specified category
-linked_true_cat_1_n = len(df_linked_true_cat_1)
+linked_true_cat_n = len(df_linked_true_cat)
 
 # Number of missed matches within specified category
-unlinked_false_cat_1_n = len(df_unlinked_false_cat_1)
+unlinked_false_cat_n = len(df_unlinked_false_cat)
 
 
-print(f"Number of true matches in category 1: {linked_true_cat_1_n}")
-print(f"Number of missed matches in category 1: {unlinked_false_cat_1_n}")
+print(f"Number of true matches in category {category}: {linked_true_cat_n}")
+print(f"Number of missed matches in category {category}: {unlinked_false_cat_n}")
 print(f"Number of linked records: {linked_n}")

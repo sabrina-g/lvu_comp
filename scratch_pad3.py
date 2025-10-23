892af313986fdd0b354b657619fb7110a960c98a
Sabrina Golonka <sabrina.golonka@ons.gov.uk>
2025-10-23 12:32:43 +0100

    Forgot to save scratch_pad3.py prior to last commit

--- scratch_pad3.py
+++ scratch_pad3.py
@@ -9,8 +9,16 @@ linked_n = len(data[data['LinkedStatus'] == 1])
 print(linked_n)
 
 linked_true_n = len(data[(data['LinkedStatus'] == 1) & (data['LinkTruth'] == 1)])
+unlinked_false_n = len(data[(data['LinkedStatus'] == 0) & (data['LinkTruth'] == 0)])
 
-linked_true_cat_n = len(data[(data['LinkedStatus'] == 1) & (data['LinkTruth'] == 1) & (data['Category'] == category)])
+
+linked_true_cat_n = len(data[(data['LinkedStatus'] == 1) & (data['LinkTruth'] == 1)
+                              & (data['Category'] == category)])
+unlinked_false_cat_n = len(data[(data['LinkedStatus'] == 0) & (data['LinkTruth'] == 0)
+                                 & (data['Category'] == category)])
+
+prop_linked_true_cat = linked_true_cat_n / (linked_true_n)
+prop_unlinked_false_cat = unlinked_false_cat_n / (unlinked_false_n)
 
 print(linked_true_n)
 print(linked_true_cat_n)

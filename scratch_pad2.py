46adf52460a44c8dfd6080fa9a2e1df1fba5c162
Sabrina Golonka <sabrina.golonka@ons.gov.uk>
2025-10-23 09:14:12 +0100

    Made get_proportions function for effect size computation

--- scratch_pad2.py
+++ scratch_pad2.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 input_file_name = 'effect_data1'
 data = pd.read_csv('data/'+input_file_name+'.csv')
-category = 3
+category = 1
+
+# Function to create DataFrames needed for counts and proportions
 
 def make_dfs(data, category):
     # Create separate df for linked and unlinked records
@@ -24,11 +26,20 @@ def make_dfs(data, category):
 
     return df_linked, df_unlinked, df_linked_true, df_unlinked_false, df_linked_true_cat, df_unlinked_false_cat
 
+# Function to get counts needed for proportions
+# Need to add a check so it doesn't try to divide by zero later
+
 def get_counts(df_linked, df_unlinked, df_linked_true, df_unlinked_false, df_linked_true_cat, df_unlinked_false_cat):
-    
+
     # Numbers of linked and unlinked records
     linked_n = len(df_linked)
     unlinked_n = len(df_unlinked)
+
+    # Number of true matches overall
+    linked_true_n = len(df_linked_true)
+
+    # Number of missed matches overall
+    unlinked_false_n = len(df_unlinked_false)
 
     # Number of true matches within specified category
     linked_true_cat_n = len(df_linked_true_cat)
@@ -36,7 +47,28 @@ def get_counts(df_linked, df_unlinked, df_linked_true, df_unlinked_false, df_lin
     # Number of missed matches within specified category
     unlinked_false_cat_n = len(df_unlinked_false_cat)
 
+    return linked_n, unlinked_n, linked_true_n, unlinked_false_n, linked_true_cat_n, unlinked_false_cat_n
 
-#print(f"Number of true matches in category {category}: {linked_true_cat_n}")
-#print(f"Number of missed matches in category {category}: {unlinked_false_cat_n}")
-#print(f"Number of linked records: {linked_n}")
+# Function to get proportions needed for d statistic
+
+def get_proportions(linked_true_n, unlinked_false_n, linked_true_cat_n, unlinked_false_cat_n):
+
+    # Proportion of true matches within specified category / overall true matches
+    prop_linked_true_cat = linked_true_cat_n / (linked_true_n)
+
+    # Proportion of missed matches within specified category / overall missed matches
+    prop_unlinked_false_cat = unlinked_false_cat_n / (unlinked_false_cat_n)
+
+    return prop_linked_true_cat, prop_unlinked_false_cat
+    
+
+# Example usages of make_dfs, get_counts, and get_proportions functions
+
+df_linked, df_unlinked, df_linked_true, df_unlinked_false, df_linked_true_cat, df_unlinked_false_cat = make_dfs(data, category)
+linked_n, unlinked_n, linked_true_n, unlinked_false_n, linked_true_cat_n, unlinked_false_cat_n = get_counts(df_linked, df_unlinked, df_linked_true, df_unlinked_false, df_linked_true_cat, df_unlinked_false_cat)
+prop_linked_true_cat, prop_unlinked_false_cat = get_proportions(linked_true_n, unlinked_false_n, linked_true_cat_n, unlinked_false_cat_n)
+
+print(f"Number of true matches in category {category}: {linked_true_cat_n}")
+print(f"Number of missed matches in category {category}: {unlinked_false_cat_n}")
+print(f"Number of linked records: {linked_n}")
+print(f"Proportion of true matches in category {category}: {prop_linked_true_cat}")

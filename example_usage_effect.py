0c1ab364946fc5b4b91fdcb6076e0abd9a11251a
Sabrina Golonka <sabrina.golonka@ons.gov.uk>
2025-12-11 15:26:49 +0000

    Deleting the chi2 function call

--- example_usage_effect.py
+++ example_usage_effect.py
@@ -1,10 +1,5 @@
 import pandas as pd
-from lvu_comp import lvu_chi2, lvu_effect_size
-
-# Example usage of lvu_chi2 function from lvu_comp 
-
-#test_chi2 = lvu_chi2('data2', 'Category', 'LinkedStatus')
-#print(test_chi2)
+from lvu_comp import lvu_effect_size
 
 # Example usage of lvu_effect_size function from lvu_comp
 
